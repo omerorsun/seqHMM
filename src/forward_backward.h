1eb65c7c398a74c1ecd2b131d7f2b9f559b2fb54
omerorsun <35125623+omerorsun@users.noreply.github.com>
2020-05-04 11:30:40 +0400

    Update forward_backward.h

--- src/forward_backward.h
+++ src/forward_backward.h
@@ -9,13 +9,15 @@ void uvBackward(const arma::sp_mat& transition, const arma::cube& emission,
   const arma::umat& obs, arma::mat& beta, const arma::vec& scales);
 
 void internalForwardx(const arma::sp_mat& transition_t, const arma::cube& emission,
-                      const arma::mat& init, const arma::ucube& obs, arma::cube& alpha, arma::mat& scales, unsigned int threads);
+                      const arma::mat& init, const arma::ucube& obs, arma::cube& alpha, arma::cube& emission_probs, arma::mat& scales, unsigned int threads);
 void internalBackwardx(const arma::sp_mat& transition, const arma::cube& emission,
                        const arma::ucube& obs, arma::cube& beta, const arma::mat& scales, unsigned int threads);
 
 void internalForward(const arma::mat& transition, const arma::cube& emission,
-                     const arma::vec& init, const arma::ucube& obs, arma::cube& alpha, arma::mat& scales, unsigned int threads);
+                     const arma::vec& init, const arma::ucube& obs, arma::cube& alpha, arma::cube& emission_probs, arma::mat& scales, unsigned int threads);
 void internalBackward(const arma::mat& transition, const arma::cube& emission,
                       const arma::ucube& obs, arma::cube& beta, const arma::mat& scales, unsigned int threads);
 
 #endif
+
+
